0e71a11f8672cbff823abe87fa6d6ec9c251d1b9
Shantanu H. Joshi <s.joshi@ucla.edu>
2013-10-07 00:07:58 -0700

    curveutils

---
+++ curvematch/curveutils.py
@@ -0,0 +1,44 @@
+#!/usr/local/epd/bin/python
+"""Various utility functions for curves"""
+
+__author__ = "Shantanu H. Joshi"
+__copyright__ = "Copyright 2012, Shantanu H. Joshi, Brandon Ayers, \
+                 Ahmanson-Lovelace Brain Mapping Center, University of California Los Angeles"
+__email__ = "s.joshi@ucla.edu"
+
+
+def winding_number():
+    pass
+
+
+def estimate_pose():
+    pass
+
+
+def repose_curve():
+    pass
+
+
+def curve_length():
+    pass
+
+
+def compute_curvature():
+    pass
+
+
+def smooth_curve():
+    pass
+
+
+def remove_duplicate_vertices():
+    pass
+
+
+def remove_traceover_defects():
+    pass
+
+
+def resample_curve_arclength():
+    pass
+
