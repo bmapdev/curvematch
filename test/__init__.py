da7e3a1a24e055db10573464f874a323e01f76b1
Shantanu H. Joshi <s.joshi@ucla.edu>
2013-10-07 00:17:42 -0700

    Move test data under test/data.

---
+++ test/__init__.py
@@ -0,0 +1,8 @@
+"""init.py"""
+
+__author__ = "Shantanu H. Joshi"
+__copyright__ = "Copyright 2013, Shantanu H. Joshi Ahmanson-Lovelace Brain Mapping Center, \
+                 University of California Los Angeles"
+__email__ = "s.joshi@ucla.edu"
+
+#__all__ = []
