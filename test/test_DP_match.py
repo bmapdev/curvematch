ccc30ca4287439ee5bdbe9bdc0d400bdb216b2b6
Shantanu H. Joshi <s.joshi@ucla.edu>
2013-10-07 00:15:22 -0700

    Modify test_DP_match. Add testing code for plotting module

--- test/test_DP_match.py
+++ test/test_DP_match.py
@@ -1,30 +1,37 @@
 #!/usr/local/epd/bin/python
-
-"""Test Dynamic Programming for matching qshapes"""
+"""Nose Dynamic Programming for matching qshapes"""
 
 __author__ = "Shantanu H. Joshi"
-__copyright__ = "Copyright 2012, Shantanu H. Joshi, Ahmanson-Lovelace Brain Mapping Center, \
-                 University of California Los Angeles"
+__copyright__ = "Copyright 2012, Shantanu H. Joshi,  \
+                 Ahmanson-Lovelace Brain Mapping Center, University of California Los Angeles"
 __email__ = "s.joshi@ucla.edu"
 
-from qshape import qshape
+from curvematch.qshape import QShape
 from shapeio import curveio
 import time
-import DPmatch
+from curvematch import DPmatch
 import numpy as np
+from curvematch.settings import Settings
+from os import path
+from sys import stdout
 
-curve1 = 'q1_partial.ucf'
-curve2 = 'q2_partial.ucf'
+Settings.output_dir = 'test/data'
+curve1 = 'data/q1_partial.ucf'
+curve2 = 'data/q2_partial.ucf'
 
-X1,attributes,isMultilevelUCF = curveio.readcurve(curve1)
-X2,attributes,isMultilevelUCF = curveio.readcurve(curve2)
 
-q1 = qshape(X1)
-q2 = qshape(X2)
-t = time.time()
-gamma, Energy = DPmatch.matchq(q1,q2)
-np.savetxt('gamma_new.txt',gamma)
-np.savetxt('Energy_new.txt',Energy)
+def test_DP_match():
 
-elapsed = time.time() - t
-print elapsed
+    X1,attributes,isMultilevelUCF = curveio.readcurve(curve1)
+    X2,attributes,isMultilevelUCF = curveio.readcurve(curve2)
+
+    q1 = QShape(X1)
+    q2 = QShape(X2)
+    t = time.time()
+    gamma, Energy = DPmatch.matchq(q1,q2)
+    np.savetxt(path.join(Settings.output_dir, 'gamma.txt'), gamma)
+    np.savetxt(path.join(Settings.output_dir, 'Energy.txt'), Energy)
+
+    elapsed = time.time() - t
+    stdout.write('\nElapsed time ' + str(elapsed))
+    stdout.flush()
---
+++ test/test_plotting.py
@@ -0,0 +1,29 @@
+#!/usr/local/epd/bin/python
+"""Nose testing for Plotting"""
+
+__author__ = "Shantanu H. Joshi"
+__copyright__ = "Copyright 2013, Shantanu H. Joshi Ahmanson-Lovelace Brain Mapping Center, \
+                 University of California Los Angeles"
+__email__ = "s.joshi@ucla.edu"
+
+from curvematch import plotting
+from curvematch.settings import Settings
+from shapeio import curveio
+from curvematch.qshape import QShape
+
+Settings.output_dir = 'test/data'
+curve1 = 'data/curve1.ucf'
+
+
+def test_plotcurve_with_name():
+    coords,atts,ismultiUCF = curveio.readcurve(curve1)
+    q1 = QShape(coords)
+    plotting.plotcurve(q1, filename='plot_test')
+    pass
+
+
+def test_plotcurve_without_name():
+    coords,atts,ismultiUCF = curveio.readcurve(curve1)
+    q1 = QShape(coords)
+    plotting.plotcurve(q1)
+    pass
