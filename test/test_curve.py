9378c95b3ca06d8ca37fc91c4df6f16793a7b549
Shantanu H. Joshi <s.joshi@ucla.edu>
2013-10-20 20:55:28 -0700

    tests for curve

---
+++ test/test_curve.py
@@ -0,0 +1,16 @@
+#!/usr/local/epd/bin/python
+"""Test the Curve class"""
+
+__author__ = "Shantanu H. Joshi"
+__copyright__ = "Copyright 2013, Shantanu H. Joshi, Brandon Ayers, \
+                 Ahmanson-Lovelace Brain Mapping Center, University of California Los Angeles"
+__email__ = "s.joshi@ucla.edu"
+
+from curvematch.settings import Settings
+from curvematch.qshape import QShape
+from shapeio import curveio
+
+Settings.output_dir = 'test/data'
+curve1 = 'test/data/curve1.ucf'
+
+
