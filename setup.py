290b80d68db3160bb57dbcea3255c583fdf8b4c8
Shantanu H. Joshi <s.joshi@ucla.edu>
2013-10-31 10:43:10 -0700

    setup now includes Cython build

--- setup.py
+++ setup.py
@@ -1,12 +1,77 @@
+#!/usr/bin/env python
+"""Setup file"""
+
+__author__ = "Shantanu H. Joshi"
+__copyright__ = "Copyright 2013, Shantanu H. Joshi, Brandon Ayers, \
+                 Ahmanson-Lovelace Brain Mapping Center, University of California Los Angeles"
+__email__ = "s.joshi@ucla.edu"
+
 from distutils.core import setup
+from distutils.extension import Extension
+import numpy as np
+
+USE_CYTHON = False
+
+if USE_CYTHON:
+    try:
+        from Cython.Distutils import build_ext
+    except ImportError:
+        if USE_CYTHON == 'auto':
+            USE_CYTHON = False
+        else:
+            raise
+
+cmdclass = {}
+ext_modules = []
+base_dir = 'curvematch'
+
+if USE_CYTHON:
+    ext_modules += \
+        [
+        Extension("curvematch.DPmatch.DPmatchcy", sources=["curvematch/DPmatch/DPmatchcy.pyx",
+                                                           "curvematch/DPmatch/DPmatch.cpp",
+                                                           "curvematch/DPmatch/shape.cpp"],
+        include_dirs = [np.get_include(), '.'], language='c++', pyrex_gdb=True)
+        ]
+    cmdclass.update({'build_ext': build_ext })
+else:
+    ext_modules += \
+        [
+         Extension("curvematch.DPmatch.DPmatchcy", sources=["curvematch/DPmatch/DPmatchcy.cpp",
+                                                            "curvematch/DPmatch/DPmatch.cpp",
+                                                            "curvematch/DPmatch/shape.cpp"],
+         include_dirs = [np.get_include(), '.'], language='c++')]
+
 
 setup(
-    name='DPmatch',
+    name='curvematch',
     version='0.1_dev',
-    packages=['DPmatch'],
+    packages=['curvematch', 'curvematch.DPmatch'],
+    package_dir={
+        'curvematch': base_dir,
+        'curvematch.DPmatch': base_dir + "/DPmatch",
+        },
+    test_suite='nose.collector',
     url='',
-    license='',
+    license='TBD',
     author='Shantanu H. Joshi, Brandon Ayers',
     author_email='s.joshi@ucla.edu, ayersb@ucla.edu',
-    description=''
-)
+    description='Shape Matching and Analysis of Curves',
+    requires=['numpy (>=1.3.0)'],
+    cmdclass=cmdclass,
+    ext_modules=ext_modules,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: TBD',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Cython',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+    keywords='shape curves matching dynamic programming',
+    )
