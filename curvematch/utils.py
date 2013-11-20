e6bc26a89deba087b33225e2aff9b3237581cd55
ayersb <ayerz02@gmail.com>
2013-11-20 15:03:02 -0800

    Tranfered utilty functions

--- curvematch/utils.py
+++ curvematch/utils.py
@@ -31,3 +31,21 @@ def shift_vertices(p, tau):
         pn[:, :t] = p[:,-t:  ]
         pn[:,t: ] = p[:,  :-t]
     return pn
+
+
+def Find_Best_Rotation(q1,q2):
+    #assumes starting points are fixed
+    shape_q = np.shape(q1)    
+    n,T = shape_q
+    A = q1*q2.transpose()
+    U,S,V = LA.svd(A)
+    if( np.absolute(np.det(U * np.det(V) -1)) < 10*np.spacing(1) ):
+        S = np.eye(n)
+    else:
+        S = np.eye(n)
+        S[:,-1] = -S[:,-1]
+    R     = U*S*V
+    q2new = R*q2
+    return(q2new)
+
+
