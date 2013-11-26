555b203579146200c8ac9f132de663b3d84c99dc
Shantanu H. Joshi <s.joshi@ucla.edu>
2013-11-25 19:20:10 -0800

    apply formatting

--- curvematch/qshape.py
+++ curvematch/qshape.py
@@ -28,24 +28,24 @@ class QShape():
 		shape_p = np.shape(p)
 		n = shape_p[0]
 		Tcoord = shape_p[1]
-		
+
 		# Compute the gradient of all row vectors in p
 		pdiff = np.zeros(shape_p)
 		for i in xrange(n):
 		    pdiff[i,:] = np.gradient( p[i,:], 2*pi /(Tcoord) )
-		# 
+		#
 		v = np.zeros(shape_p)
 		for i in xrange(n):
 		    v[i,:] = ( 2 * pi / Tcoord ) * pdiff[i,:]
 		q = np.zeros(shape_p)
 		for i in xrange(Tcoord):
 		    q[:,i] = v[:,i]/ sqrt(LA.norm(v[:,i]))
-		
+
 		q = ProjectC(q)
 		return(q)
 
 	def to_curve(q):
-		shape_q = np.shape(q) 
+		shape_q = np.shape(q)
 		n,T = shape_q[0],shape_q[1]
 		s = np.linspace(0,2*pi,T)
 		qnorm = np.zeros(T)
@@ -69,13 +69,13 @@ class QShape():
 		T = shape_q[1]
 		dt = 0.3
 		epsilon = (1.0/60.0) * 2.0 * (pi / T )
-		
+
 		itr = 0
 		res = np.ones((1,n))
 		J = np.zeros( (n,n) )
-		
+
 		s = np.linspace(0,2*pi,T)
-		
+
 
 		qnew = q / sqrt(InnerProd_Q(q,q))
 		C = {}
@@ -83,13 +83,13 @@ class QShape():
 		    if itr > 300:
 		        print "Warning: Shape failed to project.  Geodesics will be incorrect."
 		        break
-		    
+
 		    # Compute Jacobian
 		    for i in xrange(n):
 		            for j in xrange(n):
 		                J[i,j] = 3 * np.trapz( qnew[i,:] * qnew[j,:] , s)
 		    J += np.eye(n)
-		    
+
 		    qnorm = np.zeros(T)
 		    for i in xrange(T):
 		        qnorm[i] = LA.norm(qnew[:,i])
@@ -100,11 +100,11 @@ class QShape():
 		        G[i] = np.trapz( (qnew[i,:] * qnorm) , s)
 		    res = -G
 		    C[itr] = LA.norm(res)
-		    cond_J = LA.cond(J)  
+		    cond_J = LA.cond(J)
 		    if LA.norm(res) < epsilon:
 		        qnew = qnew/sqrt(InnerProd_Q(qnew,qnew))
 		        return(qnew)
-		  
+
 		    if  np.isnan(cond_J) or np.isinf(cond_J) or (cond_J < 0.1):
 		        print '\nProjection may not be accurate\n'
 		        qnew = q
@@ -117,7 +117,7 @@ class QShape():
 		        for i in xrange(n):
 		            temp = temp + x[i] * delG[i] * dt
 		        qnew += temp
-		    
+
 		        itr += 1  #Iterator for while loop
 		qnew = qnew/sqrt(InnerProd_Q(qnew,qnew))
 		print("What?")
@@ -132,7 +132,7 @@ class QShape():
 		n = shape_q[0]
 		T = shape_q[1]
 		e = np.eye(n)
-		
+
 		Ev = np.zeros((n,T,n))
 		for i in xrange(n):
 		    Ev[:,:,i] = np.tile(e[:,i],(T,1) ).transpose()
@@ -143,7 +143,7 @@ class QShape():
 		for i in xrange(n):
 		    tmp1 = np.tile( (q[i,:] / qnorm) ,(n,1))
 		    tmp2 = np.tile( qnorm ,(n,1) )
-		    delG[i] = tmp1*q + tmp2*Ev[:,:,i] 
+		    delG[i] = tmp1*q + tmp2*Ev[:,:,i]
 		return(delG)
 
 	def Group_Action_by_Gamma(q,gamma):
@@ -164,7 +164,7 @@ class QShape():
 		p = q_to_curve(q)
 		shape_q = np.shape(p)
 		n,T = shape_q
-		
+
 		#Evaluate the arc-length function
 		pdiff = np.diff(p,1,1)
 		ds = sqrt(np.sum(pdiff**2,1)) * T
