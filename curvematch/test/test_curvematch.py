d4b1d418c0274c6977eaa4c2112016240d61eb57
Shantanu H. Joshi <s.joshi@ucla.edu>
2013-12-07 13:15:29 -0800

    add test for matching curves

--- curvematch/test/test_curvematch.py
+++ curvematch/test/test_curvematch.py
@@ -6,13 +6,20 @@ __copyright__ = "Copyright 2012, Shantanu H. Joshi, Brandon Ayers, \
                  Ahmanson-Lovelace Brain Mapping Center, University of California Los Angeles"
 __email__ = "s.joshi@ucla.edu"
 
-import curvematch
-
+# import curvematch
 from curvematch.settings import Settings
-from shapeio import curveio
-from curvematch.qshape import QShape
+# from shapeio import curveio
+# from curvematch.qshape import QShape
+from curvematch import match
 
 Settings.output_dir = 'curvematch/test/data'
 curve1 = 'curvematch/test/data/curve1.ucf'
 curve2 = 'curvematch/test/data/curve2.ucf'
+settings = Settings()
+settings.closed = False
 
+def test_curvematch():
+    geodesic = match.match_curve_pair(curve1, curve2, settings)
+
+
+test_curvematch()
