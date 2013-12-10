70becb23a2d75faf880bc2c400176b0c242bbea7
Shantanu H. Joshi <s.joshi@ucla.edu>
2013-12-09 16:56:19 -0800

    add timing. plot_path now takes filename

--- curvematch/test/test_curvematch.py
+++ curvematch/test/test_curvematch.py
@@ -13,7 +13,8 @@ from curvematch.settings import Settings
 from curvematch import match
 from curvematch import plotting
 from curvematch import geodesics
-
+import time
+from sys import stdout
 
 Settings.output_dir = 'curvematch/test/data'
 curve1 = 'curvematch/test/data/curve1.ucf'
@@ -21,9 +22,14 @@ curve2 = 'curvematch/test/data/curve2.ucf'
 settings = Settings()
 settings.closed = False
 
+
 def test_curvematch():
+    t = time.time()
     geodesic = match.match_curve_pair(curve1, curve2, settings)
     curve_path = geodesics.to_curve_path(geodesic.path)
-    plotting.plot_path(curve_path)
+    plotting.plot_path(curve_path, 'elastic_path')
+    elapsed = time.time() - t
+    stdout.write('\nElapsed time ' + str(elapsed))
+    stdout.flush()
 
 test_curvematch()
