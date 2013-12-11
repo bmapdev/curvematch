a65edbe70ae0a51894095c0bbde6a5171f587ea2
Shantanu H. Joshi <s.joshi@ucla.edu>
2013-12-10 22:57:45 -0800

    resample input curves. passed rotation flag for alignment

--- curvematch/match.py
+++ curvematch/match.py
@@ -13,16 +13,21 @@ import geodesics
 from curve import Curve
 
 
-def match_curve_pair(curvefilename1, curvefilename2, settings):
+def match_curve_pair(curvefilename1, curvefilename2, settings, rotation=False, siz=100):
+
+    c1 = Curve(file=curvefilename1)
+    c2 = Curve(file=curvefilename2)
+    c1.resample_curve_uniform(siz)
+    c2.resample_curve_uniform(siz)
 
     q1 = QShape()
-    q1.from_curve_file(curvefilename1)
     q2 = QShape()
-    q2.from_curve_file(curvefilename2)
+    q1.from_curve(c1)
+    q2.from_curve(c2)
 
     if settings.closed:
         geodesic = geodesics.compute_for_closed_curves(q1, q2, settings)
     else:
-        geodesic = geodesics.compute_for_open_curves_elastic(q1, q2, settings)
+        geodesic = geodesics.compute_for_open_curves_elastic(q1, q2, settings, rotation)
 
     return geodesic
