b765d9e15e89957716664310bd39a76a43c75c80
Brandon Ayers <ayersb@ucla.edu>
2014-02-17 16:45:03 -0800

    Change all dim and six attribute references to method calls.  Fixed the functionality of the curve.append method.

--- curvematch/curve.py
+++ curvematch/curve.py
@@ -32,7 +32,7 @@ class Curve():
     def dim(self):
         return self.coords.shape[0]
     def siz(self):
-        if self.dim() == 0:
+        if len(self.coords.shape) < 2:
             return 0
         else:
             return self.coords.shape[1]
@@ -64,9 +64,9 @@ class Curve():
         pass
 
     def length(self):
-        arc_length = np.empty((self.dim, self.siz))
+        arc_length = np.empty((self.dim(), self.siz()))
         curve_gradient = np.diff(self.coords)
-        for i in range(0, self.siz):
+        for i in range(0, self.siz()):
             arc_length[:, i] = LA.norm(curve_gradient[:, i])
         return sum(arc_length)
 
@@ -91,57 +91,73 @@ class Curve():
         pass
 
     def remove_duplicate_vertices(self):
-        epsilon = 1/20.0*1.0/self.siz
-        curve_gradient = np.zeros((self.dim, self.siz))
-        for i in xrange(self.dim):
+        epsilon = 1/20.0*1.0/self.siz()
+        curve_gradient = np.zeros((self.dim(), self.siz()))
+        for i in xrange(self.dim()):
             curve_gradient[i, :] = np.gradient(self.coords[i, :])
 
-        arc_length = np.zeros(self.siz)
-        for i in range(0, self.siz):
+        arc_length = np.zeros(self.siz())
+        for i in range(0, self.siz()):
             arc_length[i] = LA.norm(curve_gradient[:, i])
 
         duplicate_status = arc_length > epsilon  # Where False, the index is duplicate
         self.coords = self.coords[:, duplicate_status]
-        self.siz = self.coords.shape[1]
-        self.shape = self.coords.shape
 
     def remove_traceover_defects(self):
         pass
 
     def resample_curve_uniform(self, newsiz=None):
         if newsiz is None:
-            newsiz = self.siz
+            newsiz = self.siz()
 
         self.remove_duplicate_vertices()
-        curve_gradient = np.zeros((self.dim, self.siz))
-        arc_length = np.zeros(self.siz)
+        curve_gradient = np.zeros((self.dim(), self.siz()))
+        arc_length = np.zeros(self.siz())
 
-        for i in xrange(self.dim):
+        for i in xrange(self.dim()):
             curve_gradient[i, :] = np.gradient(self.coords[i, :])
 
-        for i in range(0, self.siz):
+        for i in range(0, self.siz()):
             arc_length[i] = LA.norm(curve_gradient[:, i])
 
         cumulative_arc_length = np.cumsum(arc_length)
-        newcoords = np.zeros((self.dim, newsiz))
-        for i in range(0, self.dim):
+        newcoords = np.zeros((self.dim(), newsiz))
+        for i in range(0, self.dim()):
             newcoords[i, :] = np.interp(np.transpose(np.linspace(cumulative_arc_length[0],
                                                       cumulative_arc_length[-1], newsiz)),
                                                       cumulative_arc_length, self.coords[i, :])
         self.coords = newcoords
-        self.siz = newsiz
 
     def readcurve(self,filename):
         self.coords, self.attributes, ismultiUCF = curveio.readcurve(filename)
-        self.dim, self.siz = self.coords.shape
 
-    def append_curve(self, other_curve):
-        if self.dim != other_curve.dim:
+    def append_curve(self, other_curve, reverse=True):
+        if self.dim() != other_curve.dim():
             raise ValueError("Cannot connect curves with mismatched dimensions!")
-        for col in other_curve.coords.T:
-            if col not in self.coords:
-                self.coords = np.append(self.coords, col)
-                self.siz += 1
+        #for col in other_curve.coords.T:
+         #   if col not in self.coords.T:
+          #     self.coords = np.concatenate((self.coords.T, col)).T
+           #     print col.T , '\n'
+            #print "Dupe"
+        if reverse:
+            other_coords = np.fliplr(other_curve.coords)
+        else:
+            other_coords = other_curve.coords
+        for col in other_coords.T:
+            col = np.reshape(col, (col.shape[0], 1))
+            if col not in self.coords.T:
+                self.coords = np.concatenate((self.coords, col), axis=1)
+            else:
+                print self.coords.shape, col.shape
+
+
+
+
+
+
+
+
+
                 
 
 
